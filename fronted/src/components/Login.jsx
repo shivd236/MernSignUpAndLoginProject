39013c547bb0eb01b0ae97531a086659fe0a34db
Shivanshu Dubey <146203595+shivd236@users.noreply.github.com>
2024-09-24 09:07:54 +0530

    Update Login.jsx

--- fronted/src/components/Login.jsx
+++ fronted/src/components/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:4000/user/login', { email, password });
+      const response = await axios.post('https://shiva-loginform-backend.onrender.com/user/login', { email, password });
       
       console.log(response.data);
       if (response.data.code === 200) {
